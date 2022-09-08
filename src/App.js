a0cd16de3bcbd45fb453ea34a65bb7ead99ae8a1
tung22082000 <92461940+tung22082000@users.noreply.github.com>
2022-09-09 04:44:07 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React,{useState, useEffect} from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+import './App.css';
+
+
+const App=() => {
+  const initialStates = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([initialStates]);
+  
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos]);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+         <div>
+            <Header />
+          </div>
+          <div>
+            <Form
+              input={input}
+              setInput={setInput}
+              todos={todos}
+              setTodos={setTodos}
+
+            />
+           </div>
+           <div>
+            <TodoList
+            todos={todos}
+            setTodos= {setTodos}
+
+            />
+           </div>
+       </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const TodoList = ({todos, setTodos}) => {
+
+    const handleDelete=({id})=>{
+    setTodos(todos.filter((todo)=>todo.id!==id));
+  }
+  return (
+    <div>
+      {todos.map((todo) => (
+
+          <li className="list-item"key={todo.id}>
+            <input
+            type="text"
+            value={todo.title}
+            className="list"
+            onChange={(event) => event.preventDefault()}
+            />
+            <div>
+              <button
+              className="button-delete task-button"
+              onClick={()=>handleDelete(todo)}>
+              <i className='fa fa-trash'></i>
+              </button>
+            </div>
+          </li>
+      ))}
+    </div>
+  )
+}
+
+
+export default TodoList;
